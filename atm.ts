91a41f73902b5f047d8678d852f1c1cd7115b6e2
Syed Saqlain Haider <saqlainhaider786985@gmail.com>
2024-04-13 17:04:59 +0500

    ATM Using Typescript.

--- atm.ts
+++ atm.ts
@@ -15,46 +15,56 @@ let userDetails=await inq.prompt([
         message:"Enter the amount you want to deposit : "
     }
 ]);
-
+console.clear();
 console.log(`${userDetails.userName}, congratulations you have registered yourself successfully!\nNow you can use the ATM..!`);
 let exe=true;//using to control execution.
 do{
-    let trans=await inq.prompt([{ //using to get transaction mode by the user.
-        type:"list",
-        name:"transMode",
-        message:"Select the transaction you want : ",
-        choices:["Deposit Money","Withdrawal Money","Check Balance"]
+    let pass=await inq.prompt([{
+        message:"Enter your pin : ",
+        type:"number",
+        name:"pin"
     }]);
-
-    if(trans.transMode==="Deposit Money"){
-        let amount=await inq.prompt([{
-            name:"amt",
-            type:"number",
-            message:`Enter the amount you want to ${trans.transMode} : `
+    if(pass.pin===userDetails.userPin){
+        let trans=await inq.prompt([{ //using to get transaction mode by the user.
+            type:"list",
+            name:"transMode",
+            message:"Select the transaction you want : ",
+            choices:["Deposit Money","Withdrawal Money","Check Balance"]
         }]);
-
-        userDetails.accAmt+=amount.amt;
-        console.log(`Transaction successful!..`)
-    }
-    else if(trans.transMode==="Withdrawal Money"){
-        let amount=await inq.prompt([{
-            name:"amt",
-            type:"number",
-            message:`Enter the amount you want to ${trans.transMode} : `
-        }]);
-
-        if(userDetails.accAmt<amount.amt){
-            console.log("Sorry, amount is not available!");
-        }
-        else{
-
-            userDetails.accAmt-=amount.amt;
+    
+        if(trans.transMode==="Deposit Money"){
+            let amount=await inq.prompt([{
+                name:"amt",
+                type:"number",
+                message:`Enter the amount you want to ${trans.transMode} : `
+            }]);
+    
+            userDetails.accAmt+=amount.amt;
             console.log(`Transaction successful!..`)
-        
+        }
+        else if(trans.transMode==="Withdrawal Money"){
+            let amount=await inq.prompt([{
+                name:"amt",
+                type:"number",
+                message:`Enter the amount you want to ${trans.transMode} : `
+            }]);
+    
+            if(userDetails.accAmt<amount.amt){
+                console.log("Sorry, amount is not available!");
+            }
+            else{
+    
+                userDetails.accAmt-=amount.amt;
+                console.log(`Transaction successful!..`)
+            
+            }
+        }
+        else if(trans.transMode==="Check Balance"){
+            console.log(`Your current balance is ${userDetails.accAmt}`);
         }
     }
-    else if(trans.transMode==="Check Balance"){
-        console.log(`Your current balance is ${userDetails.accAmt}`);
+    else{
+        console.log("Wrong pin!!..\nTry again..");
     }
     let userCh=await inq.prompt([{
         name:"choice",
